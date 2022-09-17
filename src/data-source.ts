6c68374f026d1223115f544c6336e3c5ea009969
paulojeffdev <paulojeff.oliveira@gmail.com>
2022-09-17 00:11:27 -0300

    Alter auth mysql credentials

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,10 +4,10 @@ import { User } from "./entity/User"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: "us-cdbr-east-06.cleardb.net/heroku_b5a591b6f2cdf4d",
     port: 3306,
-    username: "paul_jeff",
-    password: "123@abc",
+    username: "b34c3f4c816441",
+    password: "7d669314",
     database: "authenticationts",
     synchronize: true,
     logging: false,
