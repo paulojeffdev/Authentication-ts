b7edfe163528e29f7ce7e8a8f0815a4c96313dfd
paulojeffdev <paulojeff.oliveira@gmail.com>
2022-09-17 10:14:01 -0300

    Alter data-source db credentials from env

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,11 +4,11 @@ import { User } from "./entity/User"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "us-cdbr-east-06.cleardb.net",
+    host: process.env.DB_HOST,
     port: 3306,
-    username: "b34c3f4c816441",
-    password: "7d669314",
-    database: "heroku_b5a591b6f2cdf4d",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     synchronize: true,
     logging: false,
     entities: [User],
